5645c452d2a55a64546cc144526506393402722b
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2015-05-07 23:11:02 -0400

    add team plugin

--- assets/plugins/msd-custom-cpt/lib/template/client-information.php
+++ assets/plugins/msd-custom-cpt/lib/template/client-information.php
@@ -3,7 +3,7 @@
     <li>
     <?php $metabox->the_field('name'); ?>
     <label id="<?php $metabox->the_name(); ?>_label" for="<?php $metabox->the_name(); ?>">Client Name</label>
-    <div class="ginput_container"><input type="text" tabindex="1" value="<?php $metabox->the_value(); ?>" id="<?php $metabox->the_name(); ?>" name="<?php $metabox->the_name(); ?>"></div>
+    <div class="input_container"><input type="text" tabindex="1" value="<?php $metabox->the_value(); ?>" id="<?php $metabox->the_name(); ?>" name="<?php $metabox->the_name(); ?>"></div>
     </li>
 <?php endwhile; ?>
 </ul>
